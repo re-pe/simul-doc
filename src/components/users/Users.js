42428f3b7d41dc64b730fd7af9ed42843d530fa4
Aurimas <cincikas.aurimas.jp17@gmail.com>
2018-05-11 09:52:06 +0300

    created login form without sending request on submit

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,12 +1,14 @@
 import React from 'react'
 // custom
 import RegForm from './UserRegForm'
+import LoginForm from './Login'
 
 const Users = () => {
   return (
     <div>
             Users page
             <RegForm />
+      <LoginForm />
     </div>
   )
 }
