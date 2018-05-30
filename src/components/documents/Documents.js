245a8c40cfd03ec834b7547e892ec9277c225613
Aurimas <cincikas.aurimas.jp17@gmail.com>
2018-05-30 15:10:11 +0300

    added toggable Document list with hardcoded temp document items

--- src/components/documents/Documents.js
+++ src/components/documents/Documents.js
@@ -1,9 +1,41 @@
 import React from 'react';
+// import PropTypes from 'prop-types';
+import Drawer from '@material-ui/core/Drawer';
+import Button from '@material-ui/core/Button';
+import data from './docDataTemp';
 
-const Documents = () => (
-  <div>
-            Document page
-  </div>
-);
+class Documents extends React.Component {
+  state = {
+    left: false,
+  };
+
+  showDocumentList = open => () => {
+    this.setState({
+      left: open,
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <Button onClick={this.showDocumentList(true)}>Show document list</Button>
+        <Drawer open={this.state.left} onClose={this.showDocumentList(false)}>
+          <div
+            tabIndex={0}
+            role="button"
+            onClick={this.showDocumentList(false)}
+            onKeyDown={this.showDocumentList(false)}
+          >
+            {data}
+          </div>
+        </Drawer>
+      </div>
+    );
+  }
+}
+
+// TemporaryDrawer.propTypes = {
+// };
 
 export default Documents;
+
