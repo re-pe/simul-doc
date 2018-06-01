9b7ba21dc9292d52299ed72744d40463f886d85d
Aurimas <cincikas.aurimas.jp17@gmail.com>
2018-06-01 15:54:59 +0300

    document list now pemanent (not fixed style)

--- src/components/documents/EditorPage.js
+++ src/components/documents/EditorPage.js
@@ -2,9 +2,6 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import Drawer from '@material-ui/core/Drawer';
-import Button from '@material-ui/core/Button';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemText from '@material-ui/core/ListItemText';
 
 import DocumentListItem from './DocumentListItem';
 import DocumentEditor from './DocumentEditor';
@@ -15,19 +12,12 @@ const mapStateToProps = state => ({
 
 class EditorPage extends React.Component {
   state = {
-    isDocumentListVisible: false,
     selected: null,
   };
 
   onSelect = (value) => {
     this.setState({
       selected: value,
-    });
-  }
-
-  showDocumentList = isVisible => () => {
-    this.setState({
-      isDocumentListVisible: isVisible,
     });
   }
 
@@ -50,28 +40,14 @@ class EditorPage extends React.Component {
 
     return (
       <div>
-        {/* need think more how to design button and where to plcehim on later versions */}
-        <Button variant="fab" mini onClick={this.showDocumentList(true)} >
-          <i className="material-icons">
-          call_received
-          </i>
-        </Button> click to select document
-        <Drawer open={this.state.isDocumentListVisible} onClose={this.showDocumentList(false)}>
-          <div
-            tabIndex={0}
-            role="button"
-            onClick={this.showDocumentList(false)}
-            onKeyDown={this.showDocumentList(false)}
-          >
-            <ListItem divider>
-              <ListItemText primary="All documents" />
-            </ListItem>
+        <Drawer variant="permanent">
+          <div>
             {documentsListElements}
           </div>
         </Drawer>
         {selected
           ? <DocumentEditor document={selected} />
-          : <h2>No document selected</h2>}
+          : <div><h2>No document selected</h2></div>}
       </div>
     );
   }
@@ -81,19 +57,9 @@ EditorPage.defaultProps = {
   documents: [],
 };
 
-// have strange behavior if i make document proptypes:
-//    array - somtimes get warn that passed is object expected array
-//    object - somtimes get warn that passed is array expected object
-// i think its because somtimes i get empty initial values before getting
-// those from redux store. Or no. Dont wanan overthink on this when lot of
-// more important work must be done
 EditorPage.propTypes = {
-  // documents: PropTypes.oneOfType([
-  //   PropTypes.arrayOf(PropTypes.object),
-  //   PropTypes.object,
-  // ]),
   documents: PropTypes.arrayOf(PropTypes.object),
 };
+
 const DocumentsWithStore = connect(mapStateToProps)(EditorPage);
 export default DocumentsWithStore;
-
