5aa3c4aebf2d9950c0d9c92d6e1b3fd341a653f0
Rėdas Peškaitis <redas.peskaitis@gmail.com>
2018-06-05 11:47:08 +0300

    Merge stashes from  document-editor

--- src/components/documents/DocumentEditor.js
+++ src/components/documents/DocumentEditor.js
@@ -1,34 +1,72 @@
 import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import { withStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
 
 const mapStateToProps = state => ({
   selected: state.documentReducer.selected,
 });
 
+const styles = theme => ({
+  container: {
+    display: 'flex',
+    flexWrap: 'wrap',
+  },
+  textField: {
+    marginLeft: theme.spacing.unit,
+    marginRight: theme.spacing.unit,
+    width: 200,
+  },
+  menu: {
+    width: 200,
+  },
+});
+
+
 const DocumentEditor = (props) => {
   const { selected } = props;
+  const { classes } = props;
   let content = <div>No document selected</div>;
   if (selected) {
     content = (
       <Fragment>
-        <div>Document id: {selected._id}</div>
-        <div>Document title: {selected.title}</div>
-        <div>Document content: {selected.content}</div>
-        <div>Document owner: {selected.owner.firstName}</div>
-        <div>Document authors: {selected.authors.reduce((curr, next) => `${curr + next.firstName} `, '') }</div>
-        <div>Document created at: {selected.createdAt}</div>
-        <div>Document updated at: {selected.createdAt}</div>
-      </Fragment>
+        <TextField
+          className={classes.textField}
+          label="Title"
+          value={selected.title}
+        />
+        <TextField
+          className={classes.textField}
+          label="Owner"
+          value={selected.owner.firstName}
+        />
+        <TextField
+          className={classes.textField}
+          label="Authors:"
+          value={selected.authors.reduce((curr, next) =>
+            `${curr + next.firstName} `, '')}
+        />
+        <TextField
+          className={classes.textField}
+          label="Created at:"
+          value={selected.createdAt}
+        />
+        <TextField
+          className={classes.textField}
+          label="Updated at:"
+          value={selected.updatedAt}
+        />
+        <TextField
+          className={classes.textField}
+          label="Content"
+          value={selected.content}
+        />
+      </Fragment >
     );
   }
   return (
-    <div className="content">
-      <h3>Not putting much effort to this element,
-      because Redas has his own idea how should this element look,
-      so just for testing purpose to check or correct props reaches this element,
-      and our document selection side bar works correctly
-      </h3>
+    <div className={classes.container}>
       {content}
     </div>);
 };
@@ -40,7 +78,8 @@ DocumentEditor.propTypes = {
   // not sure about validating document, is enough?
   // or should i use shape to validate each field in object
   selected: PropTypes.objectOf(PropTypes.any),
+  classes: PropTypes.objectOf(PropTypes.object).isRequired,
 };
 
 const DocumentEditorWithStore = connect(mapStateToProps)(DocumentEditor);
-export default DocumentEditorWithStore;
+export default withStyles(styles)(DocumentEditorWithStore);
