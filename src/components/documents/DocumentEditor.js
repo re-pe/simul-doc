d363f58ba0a44a9078e8b7c65e004a9e6407fc98
Rėdas Peškaitis <redas.peskaitis@gmail.com>
2018-06-06 17:10:59 +0300

    Replace TextFields with Typography

--- src/components/documents/DocumentEditor.js
+++ src/components/documents/DocumentEditor.js
@@ -1,5 +1,6 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, Component } from 'react';
 import PropTypes from 'prop-types';
+import Typography from '@material-ui/core/Typography';
 import { connect } from 'react-redux';
 import { withStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
@@ -11,7 +12,13 @@ const mapStateToProps = state => ({
 const styles = theme => ({
   container: {
     display: 'grid',
-    gridTemplateColumns: 'auto',
+    gridTemplateAreas: '"title title" "auto auto"  "auto auto" "content content"',
+  },
+  title: {
+    gridArea: 'title',
+  },
+  content: {
+    gridArea: 'content',
   },
   textField: {
     marginLeft: theme.spacing.unit,
@@ -23,52 +30,44 @@ const styles = theme => ({
   },
 });
 
-
-const DocumentEditor = (props) => {
-  const { selected, classes } = props;
-  let content = <div>No document selected</div>;
-  if (selected) {
-    content = (
-      <Fragment>
-        <TextField
-          className={classes.textField}
-          label="Title"
-          value={selected.title}
-        />
-        <TextField
-          className={classes.textField}
-          label="Owner"
-          value={selected.owner.firstName}
-        />
-        <TextField
-          className={classes.textField}
-          label="Authors:"
-          value={selected.authors.reduce((curr, next) =>
-            `${curr + next.firstName} `, '')}
-        />
-        <TextField
-          className={classes.textField}
-          label="Created at:"
-          value={selected.createdAt}
-        />
-        <TextField
-          className={classes.textField}
-          label="Updated at:"
-          value={selected.updatedAt}
-        />
-        <TextField
-          className={classes.textField}
-          label="Content"
-          value={selected.content}
-        />
-      </Fragment >
-    );
+class DocumentEditor extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
   }
-  return (
-    <div className={classes.container}>
-      {content}
-    </div>);
-};
+
+  render() {
+    const { selected, classes } = this.props;
+    let content = <div>No document selected</div>;
+    if (selected) {
+      content = (
+        <Fragment>
+          <Typography paragraph className={classes.title}>Title: {selected.title}</Typography>
+          <Typography paragraph>Owner: {selected.owner.firstName}</Typography>
+          <Typography paragraph>Authors:
+            {selected.authors.reduce((curr, next) => `${curr + next.firstName} `, '')}
+          </Typography>
+          <Typography paragraph>Created at: {selected.createdAt}</Typography>
+          <Typography paragraph>Updated at: {selected.updatedAt}</Typography>
+          <TextField
+            classes={{
+              textField: classes.textField,
+              content: classes.content,
+            }}
+            label="Content"
+            defaultValue={selected.content}
+            multiline
+            margin="normal"
+          />
+        </Fragment>
+      );
+    }
+    return (
+      <div className={classes.container}>
+        {content}
+      </div>);
+  }
+}
 
 DocumentEditor.defaultProps = {
   selected: undefined,
