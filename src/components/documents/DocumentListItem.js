468437ea69174a367280d3934db53274ac2053ae
Aurimas <cincikas.aurimas.jp17@gmail.com>
2018-06-07 10:51:43 +0300

    ability to delete documents

--- src/components/documents/DocumentListItem.js
+++ src/components/documents/DocumentListItem.js
@@ -1,17 +1,39 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import axios from 'axios';
+import DeleteIcon from '@material-ui/icons/Delete';
+import IconButton from '@material-ui/core/IconButton';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
+import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
+
 
 import store from '../../js/store/store';
-import { loadDocument } from '../../js/actions/document-actions';
+import { loadDocument, loadDocumentList } from '../../js/actions/document-actions';
+import { URL } from '../../js/constants/constants';
 
 const moment = require('moment');
 
 class DocumentListItem extends Component {
   selectDocument = () => {
-    // i feel its cheating (need go for mapdispatchprops.....). Or not :/
     store.dispatch(loadDocument(this.props.id));
+  }
+
+  deleteDocument = () => {
+    axios
+      .delete(`${URL}/documents/${this.props.id}`)
+      .then((response) => {
+        if (response.status === 204) {
+          // what is better after delete just resync document list from backend using loadDocsAction
+          // or should new action created that removes document from redux store
+          //  without request to backend
+          store.dispatch(loadDocumentList());
+        }
+      })
+      .catch((error) => {
+        // we should make abstract element for error to diplay error
+        console.log(error);
+      });
   }
 
   render() {
@@ -21,6 +43,11 @@ class DocumentListItem extends Component {
           primary={this.props.title}
           secondary={`${moment(this.props.created).format('YYYY-MM-DD')} (${moment(this.props.created).fromNow()})`}
         />
+        <ListItemSecondaryAction>
+          <IconButton aria-label="Delete" onClick={this.deleteDocument}>
+            <DeleteIcon />
+          </IconButton>
+        </ListItemSecondaryAction>
       </ListItem>
     );
   }
