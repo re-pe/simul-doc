3dfd43668c0782a5b8f042b1926bc4800a9d2f95
Aurimas <cincikas.aurimas.jp17@gmail.com>
2018-06-07 11:46:12 +0300

    added confirmation dialog for document delete

--- src/components/documents/DocumentListItem.js
+++ src/components/documents/DocumentListItem.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import PropTypes from 'prop-types';
 import axios from 'axios';
 import DeleteIcon from '@material-ui/icons/Delete';
@@ -6,6 +6,12 @@ import IconButton from '@material-ui/core/IconButton';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
 import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
 
 
 import store from '../../js/store/store';
@@ -15,11 +21,24 @@ import { URL } from '../../js/constants/constants';
 const moment = require('moment');
 
 class DocumentListItem extends Component {
+  state = {
+    open: false,
+  };
+
+  handleClickOpen = () => {
+    this.setState({ open: true });
+  };
+
+  handleClose = () => {
+    this.setState({ open: false });
+  };
+
   selectDocument = () => {
     store.dispatch(loadDocument(this.props.id));
   }
 
   deleteDocument = () => {
+    this.handleClose();
     axios
       .delete(`${URL}/documents/${this.props.id}`)
       .then((response) => {
@@ -38,17 +57,41 @@ class DocumentListItem extends Component {
 
   render() {
     return (
-      <ListItem button divider onClick={this.selectDocument}>
-        <ListItemText
-          primary={this.props.title}
-          secondary={`${moment(this.props.created).format('YYYY-MM-DD')} (${moment(this.props.created).fromNow()})`}
-        />
-        <ListItemSecondaryAction>
-          <IconButton aria-label="Delete" onClick={this.deleteDocument}>
-            <DeleteIcon />
-          </IconButton>
-        </ListItemSecondaryAction>
-      </ListItem>
+      <Fragment >
+        <Dialog
+          open={this.state.open}
+          onClose={this.handleClose}
+          aria-labelledby="alert-dialog-title"
+          aria-describedby="alert-dialog-description"
+        >`
+          <DialogTitle id="alert-dialog-title">Confirm deletion</DialogTitle>
+          <DialogContent>
+            <DialogContentText id="alert-dialog-description">
+              Delete the document {this.props.title}?
+            </DialogContentText>
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={this.handleClose}>
+              No
+            </Button>
+            <Button onClick={this.deleteDocument} autoFocus>
+              Yes
+            </Button>
+          </DialogActions>
+        </Dialog>
+
+        <ListItem button divider onClick={this.selectDocument}>
+          <ListItemText
+            primary={this.props.title}
+            secondary={`${moment(this.props.created).format('YYYY-MM-DD')} (${moment(this.props.created).fromNow()})`}
+          />
+          <ListItemSecondaryAction>
+            <IconButton aria-label="Delete" onClick={this.handleClickOpen}>
+              <DeleteIcon />
+            </IconButton>
+          </ListItemSecondaryAction>
+        </ListItem>
+      </Fragment>
     );
   }
 }
