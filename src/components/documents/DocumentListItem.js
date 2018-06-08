2e0cac267c0a3b5360edb8f1c9f969187dd374fb
Aurimas <cincikas.aurimas.jp17@gmail.com>
2018-06-08 14:34:51 +0300

    moved delete confirm dialog to editor page

--- src/components/documents/DocumentListItem.js
+++ src/components/documents/DocumentListItem.js
@@ -6,71 +6,30 @@ import IconButton from '@material-ui/core/IconButton';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
 import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
-import Button from '@material-ui/core/Button';
-import Dialog from '@material-ui/core/Dialog';
-import DialogActions from '@material-ui/core/DialogActions';
-import DialogContent from '@material-ui/core/DialogContent';
-import DialogContentText from '@material-ui/core/DialogContentText';
-import DialogTitle from '@material-ui/core/DialogTitle';
 
-import { deleteDocument, loadDocument } from '../../js/actions/document-actions';
+import { loadDocument } from '../../js/actions/document-actions';
 
 const moment = require('moment');
 
 class DocumentListItem extends Component {
-  state = {
-    open: false,
-  };
-
-  handleClickOpen = () => {
-    this.setState({ open: true });
-  };
-
-  handleClose = () => {
-    this.setState({ open: false });
-  };
-
   selectDocument = () => {
     this.props.selectDocument(this.props.id);
   }
 
   deleteDocument = () => {
-    this.handleClose();
-    this.props.deleteDocument(this.props.id);
+    this.props.openConfirmDialog(this.props.id, this.props.title);
   }
 
   render() {
     return (
       <Fragment >
-        <Dialog
-          open={this.state.open}
-          onClose={this.handleClose}
-          aria-labelledby="alert-dialog-title"
-          aria-describedby="alert-dialog-description"
-        >`
-          <DialogTitle id="alert-dialog-title">Confirm deletion</DialogTitle>
-          <DialogContent>
-            <DialogContentText id="alert-dialog-description">
-              Delete the document {this.props.title}?
-            </DialogContentText>
-          </DialogContent>
-          <DialogActions>
-            <Button onClick={this.handleClose}>
-              No
-            </Button>
-            <Button onClick={this.deleteDocument} autoFocus>
-              Yes
-            </Button>
-          </DialogActions>
-        </Dialog>
-
         <ListItem button divider onClick={this.selectDocument}>
           <ListItemText
             primary={this.props.title}
             secondary={`${moment(this.props.created).format('YYYY-MM-DD')} (${moment(this.props.created).fromNow()})`}
           />
           <ListItemSecondaryAction>
-            <IconButton aria-label="Delete" onClick={this.handleClickOpen}>
+            <IconButton aria-label="Delete" onClick={this.deleteDocument}>
               <DeleteIcon />
             </IconButton>
           </ListItemSecondaryAction>
@@ -84,12 +43,11 @@ DocumentListItem.propTypes = {
   id: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
   created: PropTypes.string.isRequired,
-  deleteDocument: PropTypes.func.isRequired,
+  openConfirmDialog: PropTypes.func.isRequired,
   selectDocument: PropTypes.func.isRequired,
 };
 
 const mapDispatchToProps = dispatch => ({
-  deleteDocument: id => dispatch(deleteDocument(id)),
   selectDocument: id => dispatch(loadDocument(id)),
 });
 
--- src/components/documents/EditorPage.js
+++ src/components/documents/EditorPage.js
@@ -1,18 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import Drawer from '@material-ui/core/Drawer';
 import { withStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
 
 import DocumentListItem from './DocumentListItem';
 import DocumentEditor from './DocumentEditor';
 import { HEADER_HEIGHT, FOOTER_HEIGHT } from '../../js/constants/constants';
 import CreateDocument from './AddNewDocumentButton';
-
+import { deleteDocument } from '../../js/actions/document-actions';
 
 const mapStateToProps = state => ({
   documents: state.documentReducer.documents,
 });
+
+const mapDispatchToProps = dispatch => ({
+  deleteDocument: id => dispatch(deleteDocument(id)),
+});
+
 
 const styles = () => ({
   container: {
@@ -25,32 +36,75 @@ const styles = () => ({
   },
 });
 
-const EditorPage = (props) => {
-  const { documents, classes } = props;
-  const documentsListElements = Object.values(documents).map(entry => (
-    <DocumentListItem
-      key={entry._id}
-      id={entry._id}
-      title={entry.title}
-      created={entry.createdAt}
-    />
-  ));
+class EditorPage extends Component {
+  state = {
+    open: false,
+  };
 
+  openConfirmDialog = (id, title) => {
+    this.setState({
+      open: true,
+      documentForDeleteId: id,
+      documentForDelteTitle: title,
+    });
+  };
 
-  return (
-    <div
-      className={classes.container}
-    >
-      <Drawer variant="permanent" classes={{ paper: classes.paper }}>
-        <div className="list">
-          <CreateDocument />
-          {documentsListElements}
-        </div>
-      </Drawer>
-      <DocumentEditor />
-    </div>
-  );
-};
+  closeConfirmDeletDeialog = () => {
+    this.setState({ open: false });
+  };
+
+  handleDocumentDelete = () => {
+    this.closeConfirmDeletDeialog();
+    this.props.deleteDocument(this.state.documentForDeleteId);
+  };
+
+  render() {
+    const { documents, classes } = this.props;
+    const documentsListElements = Object.values(documents).map(entry => (
+      <DocumentListItem
+        key={entry._id}
+        id={entry._id}
+        title={entry.title}
+        created={entry.createdAt}
+        openConfirmDialog={this.openConfirmDialog}
+      />
+    ));
+    return (
+      <div
+        className={classes.container}
+      >
+        <Dialog
+          open={this.state.open}
+          onClose={this.handleClose}
+          aria-labelledby="alert-dialog-title"
+          aria-describedby="alert-dialog-description"
+        >
+          <DialogTitle id="alert-dialog-title">Confirm deletion</DialogTitle>
+          <DialogContent>
+            <DialogContentText id="alert-dialog-description">
+                  Delete the {this.state.documentForDelteTitle} document ?
+            </DialogContentText>
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={this.closeConfirmDeletDeialog}>
+                  No
+            </Button>
+            <Button onClick={this.handleDocumentDelete} autoFocus>
+                  Yes
+            </Button>
+          </DialogActions>
+        </Dialog>
+        <Drawer variant="permanent" classes={{ paper: classes.paper }}>
+          <div className="list">
+            <CreateDocument />
+            {documentsListElements}
+          </div>
+        </Drawer>
+        <DocumentEditor />
+      </div>
+    );
+  }
+}
 
 EditorPage.defaultProps = {
   documents: [],
@@ -59,7 +113,8 @@ EditorPage.defaultProps = {
 EditorPage.propTypes = {
   documents: PropTypes.arrayOf(PropTypes.object),
   classes: PropTypes.objectOf(PropTypes.any).isRequired,
+  deleteDocument: PropTypes.func.isRequired,
 };
 
-const DocumentsWithStore = connect(mapStateToProps)(EditorPage);
+const DocumentsWithStore = connect(mapStateToProps, mapDispatchToProps)(EditorPage);
 export default withStyles(styles)(DocumentsWithStore);
