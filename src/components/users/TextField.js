891841c7e36348c9bb40663f49a7b3511ae369ce
Aurimas <cincikas.aurimas.jp17@gmail.com>
2018-06-12 10:16:02 +0300

    some text field changes

--- src/components/users/TextField.js
+++ src/components/users/TextField.js
@@ -1,22 +1,63 @@
-import React from 'react';
+import React, { Component } from 'react';
 import TextField from '@material-ui/core/TextField';
 
-const Input = (props) => {
-  const {
-    input,
-    label,
-    type,
-    meta: { touched, error },
-  } = props;
-  return (
-    <div className="formTextField">
-      <TextField
-        label={label}
-        type={type || 'input'}
-        {...input}
-      />
-    </div>
-  );
-};
+class ValidatingTextField extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      errorText: null,
+      validationResult: null,
+    };
+  }
 
-export default Input;
+    validate = (event) => {
+      const { value } = event.target.value;
+      const { name } = event.target.name;
+      this.setState({ value });
+      if (this.props.validationFn) {
+        const result = this.props.validationFn(value);
+        this.setState({ validationResult: result });
+      }
+    }
+
+    render() {
+      const {
+        input,
+        label,
+        type,
+        meta: { touched, error },
+      } = this.props;
+
+      return (
+        <div className="formTextField">
+          <TextField
+            label={label}
+            type={type || 'input'}
+            {...input}
+          />
+        </div>
+      );
+    }
+}
+
+
+export default ValidatingTextField;
+
+//   render () {
+//     let { name, type, hintText, floatingLabelText } = this.props
+//     let propsToSet = {
+//       name,
+//       type,
+//       hintText,
+//       floatingLabelText
+//     }
+//     return (
+//       <TextField
+//         {...propsToSet}
+//         errorText={this.state.validationResult}
+//         onChange={this.validate}
+//         onBlur={this.validate}
+//             />
+//     )
+//   }
+// }
