9bf0ab786e21a1205eb778bca1b21d45c3d42b8e
Aurimas <cincikas.aurimas.jp17@gmail.com>
2018-06-12 12:41:55 +0300

    field level validations, basic form style

--- src/components/users/Formcontainer.js
+++ src/components/users/Formcontainer.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { reduxForm, Field } from 'redux-form';
+import emailValidator from 'email-validator';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
 
 import TextField from './TextField';
 
 const required = value => (value ? undefined : 'Required');
+const minLength = value => (value && value.length < 6 ? 'Must be 6 characters or more' : undefined);
+const emailIsValid = value => (emailValidator.validate(value) ? undefined : 'Wrong email');
 
 const FormContainer = ({ handleSubmit }) => {
   // temporary -------------------
@@ -16,37 +21,41 @@ const FormContainer = ({ handleSubmit }) => {
 
   return (
     <div className="formContent">
-      <Typography variant="title" color="textSecondary" align="center">
+      <Card>
+        <CardContent>
+          <Typography variant="title" color="textSecondary" align="center">
         Registration
-      </Typography>
-      <form onSubmit={handleSubmit(submitForm)}>
-        <Field
-          name="firstName"
-          component={TextField}
-          label="First Name"
-          validate={[required]}
-        />
-        <Field
-          name="lastName"
-          component={TextField}
-          label="Last Name"
-          validate={[required]}
-        />
-        <Field
-          name="email"
-          component={TextField}
-          label="Email"
-          validate={[required]}
-        />
-        <Field
-          name="password"
-          component={TextField}
-          label="Password"
-          type="password"
-          validate={[required]}
-        />
-        <Button type="submit" variant="raised" color="inherit">Register</Button>
-      </form>
+          </Typography>
+          <form onSubmit={handleSubmit(submitForm)}>
+            <Field
+              name="firstName"
+              component={TextField}
+              label="First Name"
+              validate={[required]}
+            />
+            <Field
+              name="lastName"
+              component={TextField}
+              label="Last Name"
+              validate={[required]}
+            />
+            <Field
+              name="email"
+              component={TextField}
+              label="Email"
+              validate={[required, emailIsValid]}
+            />
+            <Field
+              name="password"
+              component={TextField}
+              label="Password"
+              type="password"
+              validate={[required, minLength]}
+            />
+            <Button type="submit" variant="raised" color="inherit">Register</Button>
+          </form>
+        </CardContent>
+      </Card>
     </div>
   );
 };
