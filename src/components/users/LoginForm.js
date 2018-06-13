5f5208c1413331392355db2ab78bff484334a9d4
Aurimas <cincikas.aurimas.jp17@gmail.com>
2018-06-13 16:15:57 +0300

    added PropTypes check for login form

--- src/components/users/LoginForm.js
+++ src/components/users/LoginForm.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { Field } from 'redux-form';
 
@@ -35,6 +36,10 @@ const LoginForm = (props) => {
       />
     </WrappedLoginForm>
   );
+};
+
+LoginForm.propTypes = {
+  login: PropTypes.func.isRequired,
 };
 
 export default connect(null, mapDispatchToProps)(LoginForm);
