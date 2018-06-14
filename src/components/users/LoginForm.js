4378ee45e05558a9b5adfa2f8430d8f70eedfdd9
Aurimas <cincikas.aurimas.jp17@gmail.com>
2018-06-14 14:43:11 +0300

    login redirects to documents now

--- src/components/users/LoginForm.js
+++ src/components/users/LoginForm.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { Field } from 'redux-form';
+import { Redirect } from 'react-router-dom';
 
 import { login } from '../../js/actions/user-actions';
 import TextField from './FormTextField';
@@ -14,37 +15,45 @@ const mapDispatchToProps = dispatch => ({
   login: user => dispatch(login(user)),
 });
 
-const LoginForm = (props) => {
-  const submitForm = (formValues) => {
-    props.login(formValues)
-      .then((result) => {
-        if (result.error) {
-          props.onAlert(result.error, 0);
-        }
-      });
-  };
+const mapStateToProps = state => ({
+  logedIn: state.userReducer.user.isLogged,
+});
 
-  return (
-    <WrappedLoginForm title="Login" buttonText="Login" submitFunction={submitForm} formName="log">
-      <Field
-        name="email"
-        component={TextField}
-        label="Email"
-        validate={[validators.validateRequired, validators.validateEmail]}
-      />
-      <Field
-        name="password"
-        component={TextField}
-        label="Password"
-        type="password"
-        validate={[validators.validateRequired, validators.validateLength(6)]}
-      />
-    </WrappedLoginForm>
-  );
-};
+class LoginForm extends Component {
+   submitForm = (formValues) => {
+     this.props.login(formValues)
+       .then((result) => {
+         if (result.error) {
+           this.props.onAlert(result.error, 0);
+         }
+       });
+   };
+   render() {
+     if (this.props.logedIn) {
+       return <Redirect to="/documents" />;
+     }
+     return (
+       <WrappedLoginForm title="Login" buttonText="Login" submitFunction={this.submitForm} formName="log">
+         <Field
+           name="email"
+           component={TextField}
+           label="Email"
+           validate={[validators.validateRequired, validators.validateEmail]}
+         />
+         <Field
+           name="password"
+           component={TextField}
+           label="Password"
+           type="password"
+           validate={[validators.validateRequired, validators.validateLength(6)]}
+         />
+       </WrappedLoginForm>
+     );
+   }
+}
 
 LoginForm.propTypes = {
   login: PropTypes.func.isRequired,
 };
 
-export default connect(null, mapDispatchToProps)(LoginForm);
+export default connect(mapStateToProps, mapDispatchToProps)(LoginForm);
